5e468eaf408eaa1906eca9f461ba20c6c6410d65
thanlinardos <thanlinardos@yahoo.gr>
2024-02-16 12:49:44 +0200

    get notification endpoint

--- src/main/java/io/nwdaf/eventsubscription/client/controller/ClientHomeController.java
+++ src/main/java/io/nwdaf/eventsubscription/client/controller/ClientHomeController.java
@@ -16,12 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -310,6 +305,12 @@ public class ClientHomeController {
         currentSubNotifications.put(safeParseLong(notification.getSubscriptionId()) + "," + safeParseLong(notification.getNotifCorrId()), notification);
         lastNotif = OffsetDateTime.now();
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping(value = "/client/notify/{id}")
+    @CrossOrigin(origins = "*")
+    public ResponseEntity<NnwdafEventsSubscriptionNotification> get(@PathVariable("id") String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(currentSubNotifications.get(id));
     }
 
     @RequestMapping(value = "/client/form")
